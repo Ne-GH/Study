14f55b6560033c5c302bc4495b9a31fe68d8041b
Ne-GH <printfne@gmail.com>
2022-07-11 11:23:58 +0800

    add a demo

--- C++/GetPassWord/Server.cpp
+++ C++/GetPassWord/Server.cpp
@@ -1,8 +1,25 @@
 #include "tool.h"
-
+#include "Info.h"
+#include "InfoManager.h"
 
 
 int main(){
+
+    InfoManager im("./test.database");
+
+    string webname,account,password,email;
+    cout << "webname:";
+    cin >> webname;
+    cout << "account:";
+    cin >> account;
+    cout << "password";
+    cin >> password;
+    cout << "email";
+    cin >> email;
+
+    Info info(webname,account,password,email);
+
+    im.ShowInfo();
 
 
 
